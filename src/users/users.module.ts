8baa383c49764f677aed24397de609ec9db59838
JsIqbal <javascriptiqbal@gmail.com>
2024-06-07 20:39:35 +0600

    Global Interceptor And Auth guard for single ROute

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,24 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { APP_INTERCEPTOR } from "@nestjs/core";
 
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
-import { User } from './user.entity';
-import { AuthService } from './auth.service';
+import { UsersService } from "./users.service";
+import { UsersController } from "./users.controller";
+import { User } from "./user.entity";
+import { AuthService } from "./auth.service";
+import { CurrentUserInterceptor } from "./interceptors/current-user.interceptor";
 
 @Module({
     imports: [TypeOrmModule.forFeature([User])],
-    providers: [UsersService, AuthService],
-    controllers: [UsersController]
+    providers: [
+        UsersService,
+        AuthService,
+
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: CurrentUserInterceptor,
+        },
+    ],
+    controllers: [UsersController],
 })
 export class UsersModule {}
