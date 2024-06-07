c709b59691141b379f7ee31110a1391f9ba3ef4b
JsIqbal <javascriptiqbal@gmail.com>
2024-06-07 18:48:03 +0600

    Complete Initial auth

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,46 +1,47 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { User } from "./user.entity";
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(User) private repo: Repository<User>){}
+    constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
     create(email: string, password: string) {
         const user = this.repo.create({ email, password });
 
-        return this.repo.save(user)
+        return this.repo.save(user);
     }
 
     findOne(id: number) {
+        if (!id) return null;
+
         return this.repo.findOneBy({ id });
     }
 
     find(email: string) {
-        return this.repo.find({ where: { email } })
+        return this.repo.find({ where: { email } });
     }
 
     async update(id: number, attars: Partial<User>) {
         const user = await this.findOne(id);
-        console.log(user)
-        
-        if(!user) {
-            throw new NotFoundException('User Not Found!'); // Not gonna work with gRPC/websockets
+
+        if (!user) {
+            throw new NotFoundException("User Not Found!"); // Not gonna work with gRPC/websockets
         }
 
         Object.assign(user, attars);
 
-        return this.repo.save(user)
+        return this.repo.save(user);
     }
 
     async remove(id: number) {
         const user = await this.findOne(id);
-        
-        if(!user) {
-            throw new NotFoundException('User Not Found!');
+
+        if (!user) {
+            throw new NotFoundException("User Not Found!");
         }
 
-        return this.repo.remove(user)
+        return this.repo.remove(user);
     }
 }
