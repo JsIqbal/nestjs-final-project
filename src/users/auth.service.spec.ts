5943658b7e9de8481c3496349926179320531057
JsIqbal <javascriptiqbal@gmail.com>
2024-06-08 19:39:23 +0600

    Create more Test For Auth Service

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -4,12 +4,14 @@ import { AuthService } from "./auth.service";
 import { UsersService } from "./users.service";
 
 import { User } from "./user.entity";
+import { BadRequestException, NotFoundException } from "@nestjs/common";
 
 describe("AuthService", () => {
     let service: AuthService;
+    let fakeUserService: Partial<UsersService>;
 
     beforeEach(async () => {
-        const fakeRepository: Partial<UsersService> = {
+        fakeUserService = {
             find: () => Promise.resolve([]),
             create: (email: string, password: string) => {
                 return Promise.resolve({ id: 1, email, password } as User);
@@ -21,7 +23,7 @@ describe("AuthService", () => {
                 AuthService,
                 {
                     provide: UsersService,
-                    useValue: fakeRepository,
+                    useValue: fakeUserService,
                 },
             ],
         }).compile();
@@ -41,5 +43,58 @@ describe("AuthService", () => {
         const [salt, hash] = user.password.split(".");
         expect(salt).toBeDefined();
         expect(hash).toBeDefined();
+    });
+
+    it("throws an error if user signs up with email that is in use", async () => {
+        try {
+            fakeUserService.find = () =>
+                Promise.resolve([
+                    {
+                        id: 1,
+                        email: "kraken@k.com",
+                        password: "1234asdf",
+                    } as User,
+                ]);
+
+            await service.signup("asdf@asdf.com", "asdf");
+        } catch (error) {
+            expect(error).toBeInstanceOf(BadRequestException);
+            expect(error.message).toContain("Email in use!");
+        }
+    });
+
+    it("throws if signin is called with an unused email", async () => {
+        try {
+            await service.signin("asdflkj@asdlfkj.com", "passdflkj");
+        } catch (error) {
+            expect(error).toBeInstanceOf(NotFoundException);
+            expect(error.message).toContain("User Not Found!");
+        }
+    });
+
+    it("throws if an invalid password is provided", async () => {
+        fakeUserService.find = () =>
+            Promise.resolve([
+                { email: "asdf@asdf.com", password: "laskdjf" } as User,
+            ]);
+        try {
+            await service.signin("laskdjf@alskdfj.com", "passowrd");
+        } catch (err) {
+            expect(err).toBeInstanceOf(BadRequestException);
+        }
+    });
+
+    it("Returns a user if password is correct", async () => {
+        fakeUserService.find = () =>
+            Promise.resolve([
+                {
+                    email: "asdf@asdf.com",
+                    password:
+                        "27348c8413b34905.4beff003437b02fc4b88302d195d77f7b2b5663f3c6fad1773005f208dc65816",
+                } as User,
+            ]);
+
+        const user = await service.signin("asdf@asdf.com", "asdf");
+        expect(user).toBeDefined();
     });
 });
