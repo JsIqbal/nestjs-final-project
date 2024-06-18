1b25d62084f82db19963d999b4b36fc9d9f1ee8b
JsIqbal <javascriptiqbal@gmail.com>
2024-06-18 14:24:34 +0600

    Complete ORM Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import {
 } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { APP_PIPE } from "@nestjs/core";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+
 const cookieSession = require("cookie-session");
 
 import { UsersModule } from "./users/users.module";
@@ -13,11 +15,10 @@ import { ReportsModule } from "./reports/reports.module";
 
 import { AppService } from "./app.service";
 
-import { User } from "./users/user.entity";
-import { Report } from "./reports/report.entity";
-
 import { AppController } from "./app.controller";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+
+import dbConfig from "./config/typeorm.config";
+// const dbConfig = require("./config/typeorm.config.js")
 
 @Module({
     imports: [
@@ -25,17 +26,7 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
             isGlobal: true,
             envFilePath: `.env.${process.env.NODE_ENV}`,
         }),
-        TypeOrmModule.forRootAsync({
-            inject: [ConfigService],
-            useFactory: (config: ConfigService) => {
-                return {
-                    type: "sqlite",
-                    database: config.get<string>("DB_NAME"),
-                    entities: [User, Report],
-                    synchronize: true,
-                };
-            },
-        }),
+        TypeOrmModule.forRoot(dbConfig),
         UsersModule,
         ReportsModule,
     ],
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,24 +1,28 @@
-var dbConfig = {
-    synchronize: false,
-};
+import { join } from "path";
+
+console.log(`Running on <${process.env.NODE_ENV}> Mode:\n`);
+
+const dbConfig = {};
 
 switch (process.env.NODE_ENV) {
     case "development":
         Object.assign(dbConfig, {
             type: "sqlite",
             database: "db.sqlite",
-            entities: ["**/*.entity.ts"], // Corrected line
+            entities: [join(__dirname, "../**/*.entity{.ts,.js}")],
+            synchronize: false,
         });
         break;
     case "test":
         Object.assign(dbConfig, {
             type: "sqlite",
             database: "test.sqlite",
-            entities: ["**/*.entity.ts"], // Corrected line
+            entities: [join(__dirname, "../**/*.entity{.ts,.js}")],
+            synchronize: true,
         });
         break;
     case "production":
-        // You might want to add a configuration for production here
+        // Add your production configuration here
         break;
     default:
         throw new Error("unknown environment");
