391c8b4dcd746e7381ed1b613b7caed815690519
JsIqbal <javascriptiqbal@gmail.com>
2024-06-20 12:45:47 +0600

    Work on Production Deployment

--- ormconfig.js
+++ ormconfig.js
@@ -13,28 +13,10 @@ var dbConfig = {
 
 switch (config.get("NODE_ENV")) {
     case "development":
-        // Object.assign(dbConfig, {
-        //     type: "sqlite",
-        //     database: "db.sqlite",
-        //     entities: ["**/*.entity.js"],
-        // });
         Object.assign(dbConfig, {
-            type: "postgres",
-            host: config.get("Hostname"),
-            port: 5432,
-            username: config.get("Username"),
-            password: config.get("Password"),
-            database: config.get("Database"),
+            type: "sqlite",
+            database: "db.sqlite",
             entities: ["**/*.entity.js"],
-            synchronize: false,
-            logging: false,
-            extra: {
-                ssl: {
-                    rejectUnauthorized: false, // Set to true if you want to enforce certificate validation
-                },
-            },
-            // Dynamically select the connection string based on the environment
-            url: config.get("External_Database_URL"),
         });
         break;
     case "test":
@@ -47,6 +29,7 @@ switch (config.get("NODE_ENV")) {
         break;
     case "production":
         Object.assign(dbConfig, {
+            // configuration for render database
             type: "postgres",
             host: config.get("Hostname"),
             port: 5432,
@@ -54,7 +37,6 @@ switch (config.get("NODE_ENV")) {
             password: config.get("Password"),
             database: config.get("Database"),
             entities: ["**/*.entity.js"],
-            synchronize: false,
             logging: false,
             extra: {
                 ssl: {
