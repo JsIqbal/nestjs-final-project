95029149fefca33279cead48bbfef9c464c7758e
JsIqbal <javascriptiqbal@gmail.com>
2024-06-20 12:45:47 +0600

    Work on Production Deployment

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,15 @@
-import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-import { ExpressAdapter } from "@nestjs/platform-express";
-import * as express from "express";
-import * as serverless from "serverless-http";
+
 import { AppModule } from "./app.module";
 
-const bootstrap = async (module: any) => {
-    const app = express();
-    const nestApp = await NestFactory.create(module, new ExpressAdapter(app));
-
-    nestApp.setGlobalPrefix("/.netlify/functions/server");
-    nestApp.enableCors();
-    nestApp.useGlobalPipes(
-        new ValidationPipe({
-            transform: true,
-        })
-    );
-    nestApp.use(express.json({ limit: "50mb" }));
-    nestApp.use(express.urlencoded({ limit: "50mb", extended: true }));
-
-    await nestApp.init();
-    return app;
-};
-
-let cachedHadler: any;
-const proxyApi = async (module: any, event: any, context: any) => {
-    if (!cachedHadler) {
-        const app = await bootstrap(module);
-        cachedHadler = serverless(app);
-    }
-
-    return cachedHadler(event, context);
-};
-
-export const handler = async (event: any, context: any) =>
-    proxyApi(AppModule, event, context);
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    (app as any).set("etag", false);
+    app.use((req, res, next) => {
+        res.removeHeader("x-powered-by");
+        res.removeHeader("date");
+        next();
+    });
+    await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
